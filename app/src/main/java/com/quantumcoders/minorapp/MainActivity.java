fb58219f3025c82efe43e3fb3de056f318e8696c
Chaitanya <1hollowpumpkin1@gmail.com>
2019-03-05 18:52:14 +0530

    Added alert dialog

--- app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
+++ app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
@@ -1,5 +1,7 @@
 package com.quantumcoders.minorapp;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
@@ -16,7 +18,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Toast.makeText(MainActivity.this,"Starting",Toast.LENGTH_SHORT).show();;
+        Toast.makeText(MainActivity.this,"Starting",Toast.LENGTH_SHORT).show();
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setMessage("Hello Hi Are you ready?").setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        }).show();
+
     }
 }
 
