7ec3988470adedfe7f270800060052f41ecb29fd
Aman Sharma <amansharmait16@acropolis.in>
2019-03-05 18:37:50 +0530

    I added button

--- app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
+++ app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //superman batman
+        //Done by punisher.. beware!
     }
 }
