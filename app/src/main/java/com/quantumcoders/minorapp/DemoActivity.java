633105a98e84e1a27fe642f740d4794c07608d41
Chaitanya <1hollowpumpkin1@gmail.com>
2019-03-05 18:32:51 +0530

    I added DemoActivity

--- app/src/main/java/com/quantumcoders/minorapp/DemoActivity.java
+++ app/src/main/java/com/quantumcoders/minorapp/DemoActivity.java
@@ -3,13 +3,11 @@ package com.quantumcoders.minorapp;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class DemoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //superman batman
+        setContentView(R.layout.activity_demo);
     }
 }
