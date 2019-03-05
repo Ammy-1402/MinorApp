e6cd0fb5df9cfa61e1154146fe8ddcf959219ba0
Aman Sharma <amansharmait16@acropolis.in>
2019-03-05 18:46:36 +0530

    Added Toast

--- app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
+++ app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.quantumcoders.minorapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //Done by punisher.. beware!
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(MainActivity.this,"Starting",Toast.LENGTH_SHORT).show();;
     }
 }
+
