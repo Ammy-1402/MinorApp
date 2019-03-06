f8d21930a021d23cece895b2211d430e438db260
Aman Sharma <amansharmait16@acropolis.in>
2019-03-06 17:25:36 +0530

    Added TabLayout With Fragments and ViewPager

--- app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
+++ app/src/main/java/com/quantumcoders/minorapp/MainActivity.java
@@ -1,21 +1,57 @@
 package com.quantumcoders.minorapp;
 
-import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.net.Uri;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements Tab1.OnFragmentInteractionListener,Tab2.OnFragmentInteractionListener{
+
+    TabLayout tabLayout;
+    ViewPager viewPager;
+    ViewPagerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tabLayout = findViewById(R.id.tabLayout);
+
+        viewPager = findViewById(R.id.id_viewPager);
+
+        //System.out.println(" --- No of tabs in tab layout --- "+tabLayout.getTabCount());
+
+        adapter = new ViewPagerAdapter(getSupportFragmentManager(),tabLayout.getTabCount());
+        //Setting adapter to view pager
+        viewPager.setAdapter(adapter);
+        viewPager.setOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
 
     }
 
     @Override
+    public void onFragmentInteraction(Uri uri) {
+
+    }
+
+
+    /*@Override
     protected void onStart() {
         super.onStart();
         Toast.makeText(MainActivity.this,"Starting",Toast.LENGTH_SHORT).show();
@@ -32,6 +68,6 @@ public class MainActivity extends AppCompatActivity {
             }
         }).show();
 
-    }
+    }*/
 }
 
---
+++ app/src/main/java/com/quantumcoders/minorapp/ViewPagerAdapter.java
@@ -0,0 +1,34 @@
+package com.quantumcoders.minorapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+    int tabCount;
+    public ViewPagerAdapter(FragmentManager fm,int tabCount) {
+        super(fm);
+        this.tabCount = tabCount;
+    }
+
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0:
+                Tab1 tab1 = new Tab1();
+                return tab1;
+            case 1:
+                Tab2 tab2 = new Tab2();
+                return tab2;
+            default:
+                tab1 = new Tab1();
+                return tab1;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
