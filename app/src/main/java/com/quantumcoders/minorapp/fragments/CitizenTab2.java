5a475f55cfa72d4758ec1e72658e867aec7eef55
Aman Sharma <amansharmait16@acropolis.in>
2019-03-31 18:28:38 +0530

    Added NEW DEMO Commit by Aman

--- app/src/main/java/com/quantumcoders/minorapp/fragments/CitizenTab2.java
+++ app/src/main/java/com/quantumcoders/minorapp/fragments/CitizenTab2.java
@@ -2,7 +2,6 @@ package com.quantumcoders.minorapp.fragments;
 
 
 import android.os.Bundle;
-import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
